db1a89c8f2f0b43873f6eab3c2ae449a3475a2cb
Johnny Noble <github-a3bd@johnny.net.au>
2020-05-28 08:33:56 +1000

    Userbase setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import userbase from "userbase-js";
 
 function App() {
+  const userbaseInit = () => {
+    console.log("① Initialising");
+    userbase
+      .init({
+        appId: "80bff789-a1b2-4f66-b931-05905ad36241",
+        // I know this shouldn't be exposed -- this is my test base which
+        // will never be prod.
+      })
+      .then((session) => {
+        if (session.user) {
+          console.log(
+            `② Session established. Username: ${session.user.username}`
+          );
+        } else {
+          console.log("② Session not established, no previous login");
+        }
+      })
+      .catch((e) => console.error(e));
+  };
+
+  const userbaseSignIn = () => {
+    console.log("③•⓪ About to sign in");
+    userbase
+      .signIn({
+        username: "johnny",
+        password: "Password123",
+      })
+      .then((user) => {
+        console.log(`③•① User signed in: ${user.username}`);
+      })
+      .catch((e) => console.error(`③•② Error: ${e}`));
+  };
+
+  const userbaseSignOut = () => {
+    console.log("④•⓪ About to sign out");
+    userbase
+      .signOut()
+      .then(() => console.log("④•① Signed out"))
+      .catch((e) => console.error(`④•② Error: ${e}`));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Console has messages, be watching there.</p>
+      <button onClick={userbaseInit}>Init (gotta do me first)</button>
+      <br />
+      <button onClick={userbaseSignIn}>Sign in</button>
+      <br />
+      <button onClick={userbaseSignOut}>Sign out</button>
     </div>
   );
 }
